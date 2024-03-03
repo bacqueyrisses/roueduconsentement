81d2ab0efb51d90ab2e4727c90bb9446cbea57eb
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-03-03 16:33:54 +0900

    feat: update hero font size (mobile)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Twitter from "@/components/icons/twitter";
 import Link from "next/link";
 import Instagram from "@/components/icons/instagram";
 
@@ -14,19 +13,19 @@ export default function Home() {
         <Instagram className="h-4 w-4 text-orange-500" />
       </Link>
       <h1
-        className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
+        className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-6xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
         style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
       >
         La roue du consentement
       </h1>
       <p
-        className="animate-fade-up mt-6 text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+        className="animate-fade-up mt-6 text-center text-lg text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
         style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
       >
         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       </p>
       <button
-        className="animate-fade-up mt-6 rounded-full bg-blue-100 px-5 py-2 font-semibold text-[#1d9bf0] transition-colors hover:bg-blue-200 md:px-7"
+        className="animate-fade-up mt-6 rounded-full bg-blue-100 px-5 py-2 text-lg font-semibold text-[#1d9bf0] transition-colors hover:bg-blue-200 md:px-7"
         style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
       >
         Tourner la roue
