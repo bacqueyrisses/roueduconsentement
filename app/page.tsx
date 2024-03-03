2ab9e90b194e381add013fd02ebe8eb0522e7cf1
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-03-03 19:45:45 +0900

    feat: reduce leading on h1

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <Instagram className="h-4 w-4 text-orange-500" />
       </Link>
       <h1
-        className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-6xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
+        className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-6xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl"
         style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
       >
         La roue du consentement
