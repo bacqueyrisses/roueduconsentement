3c5c122ce01abbee263a0710af9c4dbceb88ce36
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-03-04 13:51:27 +0900

    chore: move prettier config to package.json & run prettier

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -32,7 +32,7 @@ export const Input = forwardRef<
         "relative block w-full",
 
         // Background color + shadow applied to inset pseudo element, so shadow blends with border in light mode
-        "before:shadow before:absolute before:inset-px before:rounded-full before:bg-white",
+        "before:absolute before:inset-px before:rounded-full before:bg-white before:shadow",
 
         // Background color is moved to control and shadow is removed in dark mode so hide `before` pseudo
         "dark:before:hidden",
@@ -41,7 +41,7 @@ export const Input = forwardRef<
         "after:pointer-events-none after:absolute after:inset-0 after:rounded-full after:ring-inset after:ring-transparent sm:after:focus-within:ring-2 sm:after:focus-within:ring-blue-500",
 
         // Disabled state
-        "before:has-[[data-disabled]]:shadow-none has-[[data-disabled]]:opacity-50 before:has-[[data-disabled]]:bg-zinc-950/5",
+        "has-[[data-disabled]]:opacity-50 before:has-[[data-disabled]]:bg-zinc-950/5 before:has-[[data-disabled]]:shadow-none",
 
         // Invalid state
         "before:has-[[data-invalid]]:shadow-red-500/10",
