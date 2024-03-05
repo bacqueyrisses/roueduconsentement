289d072918f38997208c06307d5582b80d9ff265
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-03-05 14:46:29 +0900

    chore: update eslint config

--- app/wheel/page.tsx
+++ app/wheel/page.tsx
@@ -1,5 +1,6 @@
 import { signout } from "@/lib/actions/auth";
 import Check from "@/components/icons/check";
+import X from "@/components/icons/x";
 
 export default function Page() {
   return (
@@ -8,18 +9,18 @@ export default function Page() {
         <div>Est-ce que j’ai demandé à l’autre si il en a envie ?</div>
         <div className={"flex items-center justify-center gap-4"}>
           <button
-            className="mt-6 inline-flex animate-fade-up cursor-pointer items-center gap-1.5 rounded-full bg-blue-100 px-5 py-2 text-base font-medium text-blue-500 transition-colors duration-300 ease-in-out hover:bg-blue-200 hover:text-blue-600 md:px-7"
+            className="mt-6 inline-flex animate-fade-up cursor-pointer items-center gap-1.5 rounded-full bg-green-200 px-5 py-2 text-base font-medium text-green-600 transition-colors duration-300 ease-in-out hover:bg-green-300 hover:text-green-700 md:px-7"
             style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
           >
             <Check className={"size-6"} />
-            <span>Se tester maintenant</span>
+            <span>D'accord</span>
           </button>
           <button
-            className="mt-6 inline-flex animate-fade-up cursor-pointer items-center gap-1.5 rounded-full bg-blue-100 px-5 py-2 text-base font-medium text-blue-500 transition-colors duration-300 ease-in-out hover:bg-blue-200 hover:text-blue-600 md:px-7"
+            className="mt-6 inline-flex animate-fade-up cursor-pointer items-center gap-1.5 rounded-full bg-red-100 px-5 py-2 text-base font-medium text-red-500 transition-colors duration-300 ease-in-out hover:bg-red-200 hover:text-red-600 md:px-7"
             style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
           >
-            <Check className={"size-6"} />
-            <span>Se tester maintenant</span>
+            <X className={"size-6"} />
+            <span>Pas d'accord</span>
           </button>
         </div>
       </div>
