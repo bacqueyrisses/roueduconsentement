3f32af4a91903d4e047f9745b7e2bff194d54849
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-03-10 18:37:46 +0900

    feat: update wording on footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,17 +2,28 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 w-full py-5 text-center">
+    <footer className="absolute bottom-0 w-full py-5 text-center tracking-tight">
       <p className="text-gray-500">
-        Un projet de{" "}
+        Cette application a été réalisée par un collectif de chercheurs issu du
+        Master AGAPES de{" "}
         <Link
           className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
-          href="https://twitter.com/"
+          href="https://www.univ-tlse2.fr"
           target="_blank"
           rel="noopener noreferrer"
         >
-          auteur.e
+          l’université Toulouse Jean Jaurès
         </Link>
+        . Projet financé par la{" "}
+        <Link
+          className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
+          href="https://www.firah.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Firah
+        </Link>
+        .
       </p>
     </footer>
   );
