22bbaebb51d27452972ae779f7f0b23c068d0898
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-03-14 21:37:44 +0900

    chore: update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
+const env = process.env.VERCEL_ENV;
+
 const nextConfig = {
   experimental: { typedRoutes: true },
   logging: { fetches: { fullUrl: true } },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
       {
         protocol: "https",
         hostname: "avatar.vercel.sh",
@@ -16,4 +14,9 @@ const nextConfig = {
   },
 };
 
+if (env === "preview") {
+  nextConfig.typescript = {
+    ignoreBuildErrors: true,
+  };
+}
 export default nextConfig;
