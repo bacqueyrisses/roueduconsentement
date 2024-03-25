06867db98ed332f2dce17606704cfff927dba79e
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-03-25 16:38:20 +0900

    chore: update lint-staged config

--- .husky/.lintstagedrc.js
+++ .husky/.lintstagedrc.js
@@ -7,4 +7,5 @@ const buildEslintCommand = (filenames) =>
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*": "prettier --write --plugin=prettier-plugin-tailwindcss --plugin=prettier-plugin-organize-imports && node rws.js",
 };
