191eeb3431b48a1387ff5f760358ddcab4c65207
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-04-16 15:14:26 +0800

    feat: handle responsive design on questions stack

--- components/ui/question-stack.tsx
+++ components/ui/question-stack.tsx
@@ -121,7 +121,7 @@ export default function QuestionStack({
           <motion.div
             key={card.id}
             className={
-              "absolute flex h-full w-full flex-col justify-between rounded-3xl border border-neutral-200 bg-white p-4 shadow-xl shadow-black/[0.1] md:h-3/5 md:w-4/5 md:p-5"
+              "absolute flex h-2/3 w-full flex-col justify-between rounded-3xl border border-neutral-200 bg-white p-4 shadow-xl shadow-black/[0.1] md:h-3/5 md:w-4/5 md:p-5"
             }
             style={{
               transformOrigin: "top center",
@@ -140,7 +140,7 @@ export default function QuestionStack({
               </div>
               <div
                 className={
-                  "flex w-full flex-grow flex-col items-center justify-center px-12 text-center text-lg font-normal leading-relaxed text-neutral-700"
+                  "flex w-full flex-grow flex-col items-center justify-center px-2 text-center text-lg font-normal leading-relaxed text-neutral-700 md:px-12"
                 }
               >
                 {card.description}
