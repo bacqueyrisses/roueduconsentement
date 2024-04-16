2d73a56eb30de69ad93bc1494c9db3b72cef45b4
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-04-16 15:52:40 +0800

    feat: make font title smaller

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -46,7 +46,7 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
           />
           <h1
             className={
-              "inline-flex animate-fade-up flex-col bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-5xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl"
+              "inline-flex animate-fade-up flex-col bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl"
             }
             style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
           >
