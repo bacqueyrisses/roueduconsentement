5b9488993eda30039cbe2faf62fbbded17ed404b
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-05-25 16:17:21 +0200

    feat: change gradient direction and theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#F6F5FE",
+  themeColor: "#FAEDFF",
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
